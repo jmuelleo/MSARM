0f016a410cf2e936b5a5e90ef4305347144a7c9f
Julian Müller <julian.mueller5353@gmail.com>
2025-06-20 14:23:53 +0200

    Update

--- R/MSARM_plot.R
+++ R/MSARM_plot.R
@@ -1,18 +1,18 @@
 #' MSARM.plot()
 #'
-#' @param erg_MSARM.predict output from MSARM.predict
+#' @param res_MSARM.predict output from MSARM.predict
 #' @param conf if set to TRUE and MSARM.predict utilized boot = TRUE, then confidence intervals for the forecasts will be additionally plotted
 #'
 #' @return MSARM.plot allows its user to plot the forecasts from MSARM.predict to see how the time series is expected to behave in future. Furthermore
 #' MSARM.plot allows its user if conf = TRUE and boot = TRUE (in MSARM.predict) to additionally plot the bootstrap confidence intervals for the forecasts
 #' @export
 #'
-#' @examples MSARM.plot(erg_MSARM.predict,conf = TRUE)
-MSARM.plot = function(erg_MSARM.predict,conf = FALSE){
+#' @examples MSARM.plot(res_MSARM.predict,conf = TRUE)
+MSARM.plot = function(res_MSARM.predict,conf = FALSE){
 
-  Y_T = erg_MSARM.predict$Y_T
-  fcast = erg_MSARM.predict$fcast
-  n.ahead = erg_MSARM.predict$n.ahead
+  Y_T = res_MSARM.predict$Y_T
+  fcast = res_MSARM.predict$fcast
+  n.ahead = res_MSARM.predict$n.ahead
   l = length(Y_T)
 
   total_series = ts(c(Y_T,fcast))
@@ -23,7 +23,7 @@ MSARM.plot = function(erg_MSARM.predict,conf = FALSE){
   xx = c(time_new_poly,rev(time_new_poly))
 
   if(conf == TRUE){
-    fcast_boot = erg_MSARM.predict$fcast_boot
+    fcast_boot = res_MSARM.predict$fcast_boot
     for(i in 1:((dim(fcast_boot)[2]-1)/2)){
       yy = c(fcast_boot[,i+1],rev(fcast_boot[,dim(fcast_boot)[2]-i+1]))
       polygon(xx,yy,col = gray(seq(0.8, 0.3, length.out = ((dim(fcast_boot)[2]-1)/2))[i], alpha = 0.8) )
@@ -36,7 +36,7 @@ MSARM.plot = function(erg_MSARM.predict,conf = FALSE){
 
 
   if(conf == TRUE){
-    fcast_boot = erg_MSARM.predict$fcast_boot
+    fcast_boot = res_MSARM.predict$fcast_boot
     plot(ts(Y_T),lty = 1,xlim = c((T-n.ahead-50),T))
     time_new_poly = time(total_series)[(T-n.ahead+1):T]
     xx = c(time_new_poly,rev(time_new_poly))
--- R/MSARM_predict.R
+++ R/MSARM_predict.R
@@ -1,6 +1,6 @@
 #' MSARM.predict
 #'
-#' @param erg_MSARM.fit Result of an MSARM.fit estimation
+#' @param res_MSARM.fit Result of an MSARM.fit estimation
 #' @param n.ahead Number of periods to be forecasted ahead
 #' @param boot Whether bootstrapping shall be utilized (allows for creating confidence intervals)
 #' @param levels If bootstrapped, the levels for the confidence intervals
@@ -10,18 +10,19 @@
 #' boot = TRUE, in that case L bootstrap estimates and intervals will be created.
 #' @export
 #'
-#' @examples MSARM.predict(erg_MSARM.fit, n.ahead, boot = FALSE, levels = c(0.95,0.9,0.8,0.7,0.6), L = 10000)
-MSARM.predict = function(erg_MSARM.fit, n.ahead, boot = FALSE, levels = c(0.95,0.9,0.8,0.7,0.6), L = 10000){
+#' @examples MSARM.predict(res_MSARM.fit, n.ahead, boot = FALSE, levels = c(0.95,0.9,0.8,0.7,0.6), L = 10000)
+MSARM.predict = function(res_MSARM.fit, n.ahead = 1, boot = FALSE, levels = c(0.95,0.9,0.8,0.7,0.6), L = 10000){
 
-  zeta_t_T_1 = erg_MSARM.fit$zeta_t_T
+  zeta_t_T_1 = res_MSARM.fit$zeta_t_T
   n = length(zeta_t_T_1[1,])
   zeta_T_T_1 = zeta_t_T_1[,n]
 
-  P = erg_MSARM.fit$P
+  P = res_MSARM.fit$P
 
 
   Rhat = matrix(0,nrow = N, ncol = n.ahead)
   Rhat[,1] = P%*%zeta_T_T_1
+  if(n.ahead > 1){
   for(i in 2:n.ahead){
     Pm = P
     for (j in 2:i){
@@ -29,33 +30,34 @@ MSARM.predict = function(erg_MSARM.fit, n.ahead, boot = FALSE, levels = c(0.95,0
     }
     Rhat[,i] = Pm %*%zeta_T_T_1
   }
+}
 
 
-
-  alpha = erg$alpha
+  alpha = res_MSARM.fit$alpha
   K = length(alpha[1,])-2
-  coef_erg = alpha[,-(K+2)]
-  Y_T = erg_MSARM.fit$Y_T
+  coef_res = alpha[,-(K+2)]
+  Y_T = res_MSARM.fit$Y_T
 
   fcast = rep(0,n.ahead)
   Y = c(Y_T)
   n = length(Y)
   h = rep(0,N)
   for(i in 1:N){
-    h[i] = c(1,Y[n:(n-K+1)])%*%coef_erg[i,]
+    h[i] = c(1,Y[n:(n-K+1)])%*%coef_res[i,]
   }
   fcast[1] = t(h)%*%Rhat[,1]
 
+  if(n.ahead > 1){
   for(m in 2:n.ahead){
     Y = c(Y_T,fcast[1:(m-1)])
     n = length(Y)
     h = rep(0,N)
     for(i in 1:N){
-      h[i] = c(1,Y[n:(n-K+1)])%*%coef_erg[i,]
+      h[i] = c(1,Y[n:(n-K+1)])%*%coef_res[i,]
     }
     fcast[m] = t(h)%*%Rhat[,m]
   }
-
+}
 
 
   output = list(zeta_hat = Rhat,
@@ -65,7 +67,7 @@ MSARM.predict = function(erg_MSARM.fit, n.ahead, boot = FALSE, levels = c(0.95,0
 
 
 
-  residuals = erg_MSARM.fit$residuals
+  residuals = res_MSARM.fit$residuals
 
 
   if(boot == TRUE){
@@ -76,36 +78,37 @@ MSARM.predict = function(erg_MSARM.fit, n.ahead, boot = FALSE, levels = c(0.95,0
       n = length(Y)
       h = rep(0,N)
       for(i in 1:N){
-        h[i] = c(1,Y[n:(n-K+1)])%*%coef_erg[i,]
+        h[i] = c(1,Y[n:(n-K+1)])%*%coef_res[i,]
       }
       fcast_boot[1,l] = t(h)%*%Rhat[,1] + sample(residuals,1)
 
+      if(n.ahead > 1){
       for(m in 2:n.ahead){
         Y = c(Y_T,fcast[1:(m-1)])
         n = length(Y)
         h = rep(0,N)
         for(i in 1:N){
-          h[i] = c(1,Y[n:(n-K+1)])%*%coef_erg[i,] + sample(residuals,1)
+          h[i] = c(1,Y[n:(n-K+1)])%*%coef_res[i,] + sample(residuals,1)
         }
         fcast_boot[m,l] = t(h)%*%Rhat[,m]
-      }
+      }}
     }
 
     quantile_vector = c((1-levels)/2,rev(levels+(1-levels)/2))
-    erg_boot = matrix(0,nrow = n.ahead,ncol = (length(levels)*2)+1)
-    colnames(erg_boot) = c("fcast_boot",quantile_vector)
+    res_boot = matrix(0,nrow = n.ahead,ncol = (length(levels)*2)+1)
+    colnames(res_boot) = c("fcast_boot",quantile_vector)
     for(m in 1:n.ahead){
 
-      erg_boot[m,1] = mean(fcast_boot[m,])
+      res_boot[m,1] = mean(fcast_boot[m,])
 
       for(i in 1:length(quantile_vector)){
-        erg_boot[m,i+1] = quantile(fcast_boot[m,], probs = quantile_vector[i])
+        res_boot[m,i+1] = quantile(fcast_boot[m,], probs = quantile_vector[i])
       }
     }
 
     output = list(zeta_hat = Rhat,
                   fcast = fcast,
-                  fcast_boot = erg_boot,
+                  fcast_boot = res_boot,
                   Y_T = Y_T,
                   n.ahead = n.ahead)
 
