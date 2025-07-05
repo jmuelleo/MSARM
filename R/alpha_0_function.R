81665367d750792abb9b62d686c0aae1c0ca2670
Julian Müller <julian.mueller5353@gmail.com>
2025-07-05 19:41:30 +0200

    Update

--- R/alpha_0_function.R
+++ R/alpha_0_function.R
@@ -8,15 +8,15 @@
 #' @return A random starting value of alpha is generated so that the EM algorithm can start
 #' @export
 #'
-#' @examples
-#' alpha_0_function = function(Y_T,N,K)
+#' @examples alpha_0_function = function(Y_T,N,K)
+#'
 alpha_0_function = function(Y_T,N,K){
   T = length(Y_T)
   alpha = matrix(0,nrow = N,ncol = K+2)
   for(n in 1:N){
-    alpha[n,1] = runif(1,min(Y_T),max(Y_T))
-    alpha[n,2:(K+1)] = rnorm(K,0,0.5)
+    alpha[n,1] = runif(1,min(Y_T),max(Y_T)) #intercept is drawn from a uniform distribution ranging from the minimum to the maximum of the time series
+    alpha[n,2:(K+1)] = rnorm(K,0,0.5) #the coefficients are drawn from a normal distribution with mean 0 and standard deviation 0.5
   }
-  alpha[,K+2] = runif(N,0,sd(Y_T)/2) #Ist dennoch (sigma_u)^2
+  alpha[,K+2] = runif(N,0,sd(Y_T)/2) #the error term variance is first set between 0 and the standard deviation of the total time series divided by 2
   return(alpha)
 }
